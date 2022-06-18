9ddb89bba2b36a5033f58412d13e8412378d5f96
lyenheng <18839787557@163.com>
2022-06-18 21:30:53 +0800

    开始检测任务

--- src/views/CreateDetectProcedure.vue
+++ src/views/CreateDetectProcedure.vue
@@ -2,25 +2,22 @@
   <div class="create-detect-procedure">
     <div>创建检测目标</div>
 
-    <el-form ref="form" :model="formData" label-width="80px">
-      <!-- <el-form-item label="检测名称">
-        <el-input v-model="formData.name"></el-input>
-      </el-form-item>
-      <el-form-item label="检测描述">
-        <el-input v-model="formData.description"></el-input>
-      </el-form-item> -->
-      <el-form-item label="文件上传">
-        <form id="uploadForm" enctype="multipart/form-data">
-          <input
-            id="uploadFiles"
-            name="files"
-            type="file"
-            webkitdirectory
-            @change="uploadFiles"
-          />
-        </form>
-      </el-form-item>
-    </el-form>
+    <div class="detect-procedure-info">
+      <el-form ref="form" :model="detectProcedure" label-width="80px">
+        <el-form-item label="检测名称">
+          <el-input v-model="detectProcedure.name"></el-input>
+        </el-form-item>
+        <el-form-item label="检测描述">
+          <el-input v-model="detectProcedure.description"></el-input>
+        </el-form-item>
+        <el-form-item label="文件上传">
+          <form id="uploadForm" enctype="multipart/form-data">
+            <input id="uploadFiles" name="files" type="file" webkitdirectory />
+          </form>
+        </el-form-item>
+      </el-form>
+      <el-button @click="startDetect">开始检测</el-button>
+    </div>
   </div>
 </template>
 
@@ -31,19 +28,34 @@ export default {
 
   data() {
     return {
-      // formData: {
-      //   name: "test",
-      //   description: "这是测试",
-      //   files: "",
-      // },
+      detectProcedure: {
+        name: "",
+        description: "",
+      },
     };
   },
 
   methods: {
-    uploadFiles() {
+    startDetect() {
       const formData = new FormData(document.getElementById("uploadForm"));
-      formData.append("name", "test");
-      formData.append("description", "这是一条测试");
+      if (formData.get("files").size === 0) {
+        this.$message({
+          message: "请先上传文件",
+          type: "warning",
+        });
+        return;
+      }
+
+      if (this.detectProcedure.name === "") {
+        this.$message({
+          message: "请输入检测名称",
+          type: "warning",
+        });
+        return;
+      }
+
+      formData.append("name", this.detectProcedure.name);
+      formData.append("description", this.detectProcedure.description);
       startDetect(formData).then();
     },
   },
